09a2f0bf28d7e1a170d7fbb8158e5f9d4891cefb
Sergio <sergiopina30@gmail.com>
2021-05-16 16:29:35 +0200

    Initial commit

---
+++ src/main/java/com/example/toJsonParser/ToJsonParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.toJsonParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToJsonParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToJsonParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toJsonParser/ToJsonParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toJsonParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToJsonParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
